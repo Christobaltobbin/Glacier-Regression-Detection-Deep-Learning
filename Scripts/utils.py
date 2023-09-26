c4139f88976296490b966ed3e0b8a443ea6cecaf
Christobal Tobbin <116877317+Christobaltobbin@users.noreply.github.com>
2023-09-26 11:31:58 +0200

    added binary mask to main script

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -2,10 +2,12 @@ import cv2
 import os
 import tifffile # for reading tiff files. There are other modules that can do this, but tifffile is most stable on Windows
 import numpy as np # for array handling
+from matplotlib import pyplot
 import matplotlib.pyplot as plt # for QC
 import glob # to gather up image filepath lists
 import rasterio
 from rasterio.plot import show
+from PIL import Image
 import scipy # same
 import imagecodecs
 from sklearn.model_selection import train_test_split
@@ -24,7 +26,69 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_im
 from tensorflow.keras import backend as K
 # We need some additional layers already pre-defined in Keras
 
+# Function for Binary Mask 
+# Define the folder path
+def create_binary_mask(input_path, output_path):
+    
+    # List all the tiff files
+    tif_files = [f for f in os.listdir(input_path) if f.endswith('.tif')]
 
+    # Set the number of rows and columns for subplots
+    num_rows = len(tif_files)
+    num_cols = 3  # Original image + Mask
+
+    # Create a figure with subplots
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 5 * num_rows))
+
+    # Loop through each file and process it
+    for i, tif_file in enumerate(tif_files):
+        tiff_image = rasterio.open(os.path.join(input_path, tif_file))
+        array = tiff_image.read(1).astype(np.float32())
+        
+        # Display the image using matplotlib
+        # plt.imshow(array, cmap='gray')
+        # plt.title(tif_file)  # Set the title to the file name
+        # plt.show()
+        axs[i, 0].imshow(array, cmap='gray')
+        axs[i, 0].set_title("Original " + tif_file)
+        
+        masked = np.where(array <= 5000, 0, 1)
+            
+        # Create a new figure for each mask display
+        # plt.figure()    
+        # plt.imshow(masked, cmap='gray')
+        # plt.title("Mask for " + tif_file)
+        # plt.show()
+        axs[i, 1].imshow(masked, cmap='gray')
+        axs[i, 1].set_title("Mask for " + tif_file)
+        
+        from rasterio.features import sieve
+        sieved_msk = sieve(masked, size=800)
+        
+        #Create a profile to save masks
+        profile = tiff_image.profile
+        profile.update(
+            dtype=rasterio.float32,
+            count=1,
+            compress='lzw'
+        )
+        
+        #save masked images
+        mask_seived_name = os.path.splitext(tif_file)[0]+"_mask.tif"
+        mask_seived_path = os.path.join(output_path, mask_seived_name)
+        with rasterio.open(mask_seived_path, 'w', **profile) as dst:
+            dst.write(sieved_msk.astype(np.float32), 1)
+        # plt.figure()
+        # plt.imshow(sieved_msk, cmap='gray')
+        # plt.title("Sieved Mask for " + tif_file)
+        # plt.show()
+        axs[i, 2].imshow(sieved_msk, cmap='gray')
+        axs[i, 2].set_title("Sieved Mask for " + tif_file)
+    # Adjust subplot spacing
+    plt.tight_layout()
+
+    # Show the subplots
+    plt.show()
 
 # creating a function for reading the training data
 def read_data(files_path):
