1f899171bc18981d389b4ebd66c6d5334ef8e623
Christobal Tobbin <116877317+Christobaltobbin@users.noreply.github.com>
2023-09-28 20:53:19 +0200

    commented retuen toptrainlist

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -147,7 +147,7 @@ def resize_images(resize_value, no_of_channels, data_list, output_file_path):
         # Save the individual reshaped image as TIFF
         save_path = os.path.join(output_file_path, f'image_{i}.tif')
         tifffile.imwrite(save_path, img_norm)
-        return top_train_total
+        # return top_train_total
 
 def plot_label(label_list_path, index_number):
     img = tifffile.imread(label_list_path[index_number])
